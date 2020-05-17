b6f19339092bceeb2a2b0c0ec6a01cf2a81b0fc9
Henrique Tavares <ihenrits@gmail.com>
2020-05-17 15:41:37 -0300

    feat: creating order

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,55 @@ interface IRequest {
 @injectable()
 class CreateProductService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found.');
+    }
+
+    const idProducts = products.map(({ id }) => ({ id }));
+
+    const allProducts = await this.productsRepository.findAllById(idProducts);
+
+    if (products.length !== allProducts.length) {
+      throw new AppError('Products not found.');
+    }
+
+    allProducts.forEach(product => {
+      const productOrder = products.find(({ id }) => product.id === id);
+
+      if (productOrder) {
+        if (productOrder.quantity > product.quantity) {
+          throw new AppError('Product with insuficient quantity');
+        }
+      }
+    });
+
+    const arrayProducts = allProducts.map(({ id, price }) => ({
+      product_id: id,
+      price,
+      quantity: products.find(prod => prod.id === id)?.quantity || 0,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: arrayProducts,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
